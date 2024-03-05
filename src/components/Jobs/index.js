abc7c53516854a8f75a8ced8182ce980db793c5a
syamvelaga <syamvelaga64@gmail.com>
2024-03-05 05:16:38 +0000

    first commit

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -133,8 +133,13 @@ class Jobs extends Component {
   onGetJobDetails = async () => {
     this.setState({apiJobsStatus: apiJobsStatusConstants.inProgress})
     const jwtToken = Cookies.get('jwt_token')
-    const {checkboxInputs, radioInput, searchInput, locationCheckbox} =
-      this.state
+    const {
+      checkboxInputs,
+      radioInput,
+      searchInput,
+      locationCheckbox,
+    } = this.state
+
     const jobsApiUrl = `https://apis.ccbp.in/jobs?employment_type=${checkboxInputs}&minimum_package=${radioInput}&search=${searchInput}&location=${locationCheckbox}`
     const optionsJobs = {
       headers: {
